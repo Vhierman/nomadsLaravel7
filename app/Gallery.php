7eda091b8f90b3b7f7c4c3d347f998b8c01f7e0e
Vhierman <achmadfirmansyah989@yahoo.co.id>
2021-02-18 14:12:07 +0700

    Model Gallery

---
+++ app/Gallery.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+//Definisikan SoftDeletes Agar Bisa dipakai di class Travel Package
+use Illuminate\Database\Eloquent\SoftDeletes;
+//End Definisikan SoftDeletes Agar Bisa dipakai di class Travel Package
+
+class Gallery extends Model
+{
+    //Mengaktifkan SoftDeletes
+    use SoftDeletes;
+
+    //Data Yang boleh dimasukan ke dalam database
+    protected $fillable = [
+        'travel_package_id','image'
+    ];
+
+    //
+    protected $hidden = [
+
+    ];
+
+    //Membuat Relasi Antara Gallery Dengan PackageTravel
+    public function travel_package() {
+        return $this->belongsTo(TravelPackage::class,'travel-packages_id','id');
+    }
+
+}
--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,10 +5,21 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+//Memanggil Model TravelPackage
+use App\TravelPackage;
+use App\Transaction;
+
 class DashboardController extends Controller
 {
     //Method index, Haalaman Utama Admin
-    public function index(Request $request){
-        return view('pages.admin.dashboard');
+    public function index(Request $request)
+    {
+        //Menampilkan View Pada Folder pages/admin/dashboard.blade.php
+        return view('pages.admin.dashboard',[
+            //Menghitung Paket Travel
+            'travel_package' => TravelPackage::count(),
+            // 'transaction' => Transaction::count(),
+        ]);
+        
     }
 }
